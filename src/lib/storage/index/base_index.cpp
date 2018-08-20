7a635fc8763894e024bdc786c8c69cf19427005f
martin.boissier <martin.boissier@hpi.de>
2018-08-20 14:37:01 +0200

    Fix wording

--- src/lib/storage/index/base_index.cpp
+++ src/lib/storage/index/base_index.cpp
@@ -20,14 +20,14 @@ bool BaseIndex::is_index_for(const std::vector<std::shared_ptr<const BaseColumn>
 
 BaseIndex::Iterator BaseIndex::lower_bound(const std::vector<AllTypeVariant>& values) const {
   DebugAssert((_get_index_columns().size() >= values.size()),
-              "BaseIndex: The amount of queried columns has to be less or equal to the number of indexed columns.");
+              "BaseIndex: The number of queried columns has to be less or equal to the number of indexed columns.");
 
   return _lower_bound(values);
 }
 
 BaseIndex::Iterator BaseIndex::upper_bound(const std::vector<AllTypeVariant>& values) const {
   DebugAssert((_get_index_columns().size() >= values.size()),
-              "BaseIndex: The amount of queried columns has to be less or equal to the number of indexed columns.");
+              "BaseIndex: The number of queried columns has to be less or equal to the number of indexed columns.");
 
   return _upper_bound(values);
 }
